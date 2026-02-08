59d202f88fd922ea915df61a5ef4b669d455c0e8
PUNITH R <punithr.ec24@bmsce.ac.in>
2026-02-08 21:27:05 +0530

    Add files via upload

---
+++ Examples/ex3/fifo/fifo_design.sv
@@ -0,0 +1,23 @@
+  module fifo(
+  input clk, wr, rd,
+  input [7:0] din,
+  output logic full, empty
+);
+  
+  logic [4:0] cnt = 0;
+  
+  assign full = (cnt == 16);
+  assign empty = (cnt == 0);
+  
+  always_ff @(posedge clk) begin
+    if (wr && !full && !rd) 
+      cnt <= cnt + 1;
+    else if (rd && !empty && !wr) 
+      cnt <= cnt - 1;
+  end
+endmodule
+
+interface fifo_if(input clk);
+  logic wr, rd, full, empty; 
+  logic [7:0] din;
+endinterface
